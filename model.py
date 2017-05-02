d4f39a4221e4e79fc05789733f16c94b32dfb7e5
albertbou <albertbou92@gmail.com>
2017-05-02 21:58:28 +0200

    Function Parse MIDI
    Programed a function that given a MIDI file returns both the measures and the Chords/Notes, step by step in order.

--- model.py
+++ model.py
@@ -10,6 +10,10 @@ import random
 import sys
 import os
 from music21 import *
+from collections import defaultdict, OrderedDict
+from itertools import groupby, izip_longest
+import copy, random, pdb
+
 
 ################################
 #    CHECK PATH WAS PROVIDED   #
@@ -25,10 +29,77 @@ else:
 #    FILE LIST    #
 ###################
 
-numFiles = 1
+numFiles = 2
 file_names = os.listdir(path)#List of file names in the directory
 file_names = sorted(file_names, key=lambda item: (int(item.partition('.')[0]) if item[0].isdigit() else float('inf'), item))
 file_names = file_names[0:numFiles]
+
+##################
+#    FUNCTIONS   #
+##################
+
+#Obtain chords+notes merged in a single voice
+def extractOneVoice(MIDIdata,i):
+
+    # We start taking the first part
+    mainInstrument = MIDIdata[i]  # select channel 0
+    voices = mainInstrument.getElementsByClass(stream.Voice)
+    num_voices = len(voices)
+
+    # Sum up all voices
+    melody = mainInstrument.getElementsByClass(stream.Voice)[0]
+    for i in range(1, num_voices):
+        newVoice = mainInstrument.getElementsByClass(stream.Voice)[i]
+        for j in newVoice:
+            melody.insert(j.offset, j)
+
+    return melody
+
+#Obtain measures and chords from a MIDI file
+def parseMidi(filename):
+
+    MIDIdata = converter.parse(
+        path + "/" + filename)  # Parse the MIDI data for separate melody and accompaniment parts.
+    # a Score class is obtained. Socre class is a Stream subclass for handling multi-part music.
+    par = MIDIdata.parts  # Returns parts of the score. It filters out all other things that might be in a Score object, such as Metadata returning just the Parts.
+    num_parts = len(par)
+    melody = extractOneVoice(MIDIdata, 0)
+
+    # Add the other parts
+    partIndices = range(1, num_parts)
+    comp_stream = stream.Voice()
+    comp_stream.append([j.flat for i, j in enumerate(MIDIdata) if i in partIndices])
+    for i in (partIndices):
+        new_part = extractOneVoice(MIDIdata, i)
+        for j in new_part:
+            melody.insert(j.offset, j)
+
+    melody.removeByClass(note.Rest)
+    #melody.removeByClass(note.Note)
+    #melody.removeByClass(chord.Chord)
+
+    measures = OrderedDict()
+    measureNum = 0
+    for part in melody:
+        curr_part = stream.Part()
+        print(part)
+        curr_part.append(part.getContextByClass('Instrument'))
+        curr_part.append(part.getContextByClass('MetronomeMark'))
+        curr_part.append(part.getContextByClass('KeySignature'))
+        curr_part.append(part.getContextByClass('TimeSignature'))
+        measures[measureNum] = curr_part
+        measureNum += 1
+
+    chords = OrderedDict()
+    chordNum = 0
+    for part in melody:
+        curr_part = stream.Part()
+        curr_part.append(part.getContextByClass('Chord'))
+        #curr_part.append(part.getContextByClass('Note'))
+        chords[chordNum] = curr_part
+        chordNum += 1
+
+    return measures,chords
 
 ###################
 #    READ DATA    #
@@ -36,8 +107,6 @@ file_names = file_names[0:numFiles]
 
 data = []
 for i in range (len(file_names)):
-    MIDIdata = converter.parse(path + "/" + file_names[i])
-    MIDIdata.show('text')
-
+    measures,chords = parseMidi(file_names[1])
 
 
